1c8457e72b448d455de540962063437a76397277
Hristina Stoykova <hstoykova@axway.com>
2025-02-17 00:19:18 -0700

    Update

--- stSitePromotion.py
+++ stSitePromotion.py
@@ -1,14 +1,14 @@
 import json
+import logging
 import warnings
 from textwrap import indent
 
 import dotenv
-from funcsession import *
+from func import *
 
 import hvac
 
-siteToMigrate = {'hrisy': 'SMB'}
-
+siteToMigrate = [{'hrisy': 'SMB'}]
 
 # SETTINGS
 warnings.filterwarnings("ignore", category=DeprecationWarning)
@@ -16,9 +16,15 @@ dotenv_path = ".env"
 dotenv.load_dotenv(dotenv_path)
 hashiToken = os.environ.get("VAULT_TOKEN")
 hashiHost = os.environ.get("VAULT_HOST")
-s, urlST = Authenticate("ST_NON_PROD")
-s.verify = False
-s.headers.update({'Accept':'application/json'})
+
+# Authenticate with ST
+s_source, source_ST = Authenticate("ST_NON_PROD")
+s_source.verify = False
+s_source.headers.update({'Accept': 'application/json'})
+s_target, target_ST = Authenticate("ST_PROD")
+s_target.verify = False
+s_target.headers.update({'Accept': 'application/json'})
+s_target.headers.update({'Content-Type': 'application/json'})
 
 # VARS
 vault = 'SecureTransport'
@@ -33,19 +39,28 @@ client = hvac.Client(
 # EXPORT FROM SOURCE ST
 
 resource = "sites/"
-params = {'account': list(siteToMigrate)[0], 'name': siteToMigrate[list(siteToMigrate)[0]]}
-res = s.get(urlST + resource, params = params)
-sites = res.json()['result'][0]
-read_response = client.secrets.kv.v2.read_secret_version(mount_point=vault,
-                                                                 path=f'accounts/{list(siteToMigrate)[0]}/sites/{tier}/{siteToMigrate[list(siteToMigrate)[0]]}')
+for sites in siteToMigrate:
+    for k, v in sites.items():
+        params = {'account': k, 'name': v}
+        res = s_source.get(source_ST + resource, params=params)
+        site = res.json()['result'][0]
+        path = f'accounts/{k}/sites/{tier}/{v}'
+        read_response = read_secret_version(client, vault, path)
+        metadata = read_response['data']['metadata'].get('custom_metadata', {})
+        data = read_response['data'].get('data', {})
+        if site['type'] == 'ExternalPersistedCustomSite':
+            update_custom_properties(site, metadata, data)
+        else:
+            update_site_properties(site, metadata, data)
 
-
-
-sess, targetST = Authenticate("ST_PROD")
-resource = 'sites/'
-
-if sites:
-    sites['smbUploadFolder'] = read_response['data']['metadata']['custom_metadata']['smbUploadFolder']
-    sites['smbPassword'] = read_response['data']['data']['smbPassword']
-    response = sess.post(targetST + resource, json=sites)
-    logging.info(f"Site migration status {response.status_code}; Message: {response.text}")
+        checkIfExists = s_target.get(target_ST + resource, params={'account': k,
+                                                                   'name': v})
+        checkIfExistsResult = checkIfExists.json()
+        if checkIfExistsResult['resultSet']['totalCount'] == 0:
+            response = s_target.post(target_ST + resource, json=site)
+            if response.ok:
+                logging.info(f"Site migration status {response.status_code}; Message: {response.text}")
+            else:
+                print(response.text)
+        else:
+            logging.info(f"Site {site['name']} already exists")
